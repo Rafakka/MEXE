c88f4ef3f44d61277b8a83d3fd102e8802c5f557
Rafakka <rafaelkaher@gmail.com>
2026-07-20 16:14:28 -0300

    refinar efeitos de result

--- frontend/src/components/Laboratory/ReactionPanel/ReactionPanel.tsx
+++ frontend/src/components/Laboratory/ReactionPanel/ReactionPanel.tsx
@@ -18,18 +18,41 @@ export default function ReactionPanel({phase}:ReactionPanelProps){
             `}
         >
 
-        <div className={styles.connector} />
-        <div className={styles.surface}>
-        <div className={styles.preview}>
+        <div className={styles.connector} >
+
+            <div className={styles.line}></div>
+
+            <div className={styles.line}></div>
+
+            <div className={styles.line}></div>
 
         </div>
 
-        <div className={styles.metadata}>
+        <div className={styles.surface}>
 
-            <span>1024 x 7268</span>
-            <span>Png</span>
-            <span>RGB</span>
-            <span>1.2 MB</span>
+            <div className={styles.preview}>
+
+            </div>
+
+            <div className={styles.metadata}>
+                <span>1024 x 7268</span>
+                <span>Png</span>
+                <span>RGB</span>
+                <span>1.2 MB</span>
+            </div>
+
+            <div className={styles.scanner}></div>
+
+            <div className={styles.borderFlow}>
+
+            <div className={styles.top}></div>
+
+            <div className={styles.right}></div>
+
+            <div className={styles.bottom}></div>
+
+            <div className={styles.left}></div>
+
         </div>
 
     </div>
