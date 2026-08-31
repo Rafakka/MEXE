96f08c36bc5dfc512e2f10521abb4a3d4ef3fafe
Rafakka <rafaelkaher@gmail.com>
2026-08-31 15:57:26 -0300

    debugging security test ci

--- scripts/security-scan.sh
+++ scripts/security-scan.sh
@@ -1,87 +1,106 @@
-
 #!/usr/bin/env bash
 
 set -euo pipefail
+set -x
 
 TRIVY_IMAGE="aquasec/trivy:latest"
+
+trap 'echo; echo "❌ Security Scan FAILED"; read -rp "Pressione ENTER para fechar..."' ERR
 
 echo "== MEXE Security Scan =="
 
 echo
 echo "[1/6] Backend"
+
 docker run --rm \
-  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v //var/run/docker.sock:/var/run/docker.sock \
   "$TRIVY_IMAGE" \
   image \
   --severity HIGH,CRITICAL \
+  --exit-code 0 \
+  mexe-backend:latest
+
+docker run --rm \
+  -v //var/run/docker.sock:/var/run/docker.sock \
+  "$TRIVY_IMAGE" \
+  image \
+  --severity CRITICAL \
   --exit-code 1 \
-  IMAGEM
   mexe-backend:latest
 
 echo
 echo "[2/6] Frontend"
+
 docker run --rm \
-  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v //var/run/docker.sock:/var/run/docker.sock \
   "$TRIVY_IMAGE" \
   image \
   --severity HIGH,CRITICAL \
+  --exit-code 0 \
+  mexe-frontend:latest
+
+docker run --rm \
+  -v //var/run/docker.sock:/var/run/docker.sock \
+  "$TRIVY_IMAGE" \
+  image \
+  --severity CRITICAL \
   --exit-code 1 \
-  IMAGEM
   mexe-frontend:latest
 
 echo
 echo "[3/6] Alloy"
-docker pull grafana/alloy:latest
+
+docker pull grafana/alloy:v1.19.1
 
 docker run --rm \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v "$PWD/.trivyignore:/root/.trivyignore:ro" \
+  -v //var/run/docker.sock:/var/run/docker.sock \
+  -v "$PWD/.trivyignore://root/.trivyignore:ro" \
   "$TRIVY_IMAGE" \
   image \
   --severity CRITICAL \
   --exit-code 1 \
-  --ignorefile /root/.trivyignore \
-  IMAGEM
-  grafana/alloy:latest
+  --ignorefile //root/.trivyignore \
+  grafana/alloy:v1.19.1
 
 echo
 echo "[4/6] Grafana"
-docker pull grafana/grafana:latest
+
+docker pull grafana/grafana:13.2.0
 
 docker run --rm \
-  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v //var/run/docker.sock:/var/run/docker.sock \
   "$TRIVY_IMAGE" \
   image \
   --severity CRITICAL \
   --exit-code 1 \
-  IMAGEM
-  grafana/grafana:latest
+  grafana/grafana:13.2.0
 
 echo
 echo "[5/6] Loki"
+
 docker pull grafana/loki:3.7.7
 
 docker run --rm \
-  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v //var/run/docker.sock:/var/run/docker.sock \
   "$TRIVY_IMAGE" \
   image \
   --severity CRITICAL \
   --exit-code 1 \
-  IMAGEM
   grafana/loki:3.7.7
 
 echo
 echo "[6/6] Prometheus"
-docker pull prom/prometheus:latest
+
+docker pull prom/prometheus:v3.14.0
 
 docker run --rm \
-  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v //var/run/docker.sock:/var/run/docker.sock \
   "$TRIVY_IMAGE" \
   image \
   --severity CRITICAL \
   --exit-code 1 \
-  IMAGEM
-  prom/prometheus:latest
+  prom/prometheus:v3.14.0
 
 echo
 echo "== Security Scan PASSED =="
+read -rp "Pressione ENTER para fechar..."
